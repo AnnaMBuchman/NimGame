e7d3480696844939f6af0be9b1b8253ac7e6df9d
Anna Buchman <01130560@pw.edu.pl>
2021-03-15 20:34:53 +0100

    Adding stack class

---
+++ NimGame/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NimGame
+{
+    public class Stack
+    {
+        private int number { get; }
+        private int numberOfElements { get; set; }
+        public Stack(int number, int numberOfElements)
+        {
+            this.number=number;
+            this.numberOfElements = numberOfElements;
+        }
+
+        public void takeNumberOfElements(int takenElements)
+        {
+            if (numberOfElements >= takenElements)
+                numberOfElements -= takenElements;
+            else
+                throw new ArgumentException("to large number of takenElements");
+        }
+        public bool checkIfEmpty()
+        {
+            if (numberOfElements == 0)
+                return true;
+            return false;
+        }
+        public bool canBeTaken(int elementsToTake)
+        {
+            if (numberOfElements >= elementsToTake)
+                return true;
+            return false;
+        }
+    }
+}
