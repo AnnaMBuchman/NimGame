49ac925c4a6a1b0de49406d3b2d68fa52d29a630
Anna Buchman <01130560@pw.edu.pl>
2021-03-15 20:46:21 +0100

    Adding test project

---
+++ NUnitTestNimGame/UnitTestStack.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using NimGame;
+
+namespace NUnitTestNimGame
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void takingElementsFromStack()
+        {
+            Stack s = new Stack(3, 4);
+            Assert.IsTrue(s.canBeTaken(2));
+            Assert.IsFalse(s.canBeTaken(5));
+            Assert.IsTrue(s.canBeTaken(4));
+            s.takeNumberOfElements(2);
+            Assert.IsTrue(s.canBeTaken(2));
+            Assert.IsFalse(s.canBeTaken(5));
+            Assert.IsFalse(s.canBeTaken(4));
+        }
+    }
+}
